d2041572a2b03b6866108dea6253c9bfe9c4de94
Aciole.D <aciole.d@gmail.com>
2025-07-22 21:50:51 -0300

    update prettyseq
    now growing seqs are shown

--- lamp-pretty-seq-prettyseq-loop.R
+++ lamp-pretty-seq-prettyseq-loop.R
@@ -63,15 +63,132 @@ for (setnum in 'Set-9') {
   atitleposition<-nchar(sequence)/2
   
   tagplaces<-(tab2[,grep('stt', names(tab2))])
-  names(tagplaces)<-gsub(x = names(tagplaces),pattern = '.stt',replacement = '')
-  tagseqs<-as.vector(tab2[,grep('seq', names(tab2))])
-  tagcenter<-(tab2[,grep('stt', names(tab2))])+(tab2[,grep('len', names(tab2))]/2)
+  tagplaces<-as.data.frame(t(tagplaces))
+  names(tagplaces)<-'stt'
+  tagplaces[,2]<-as.data.frame(t(tab2[,grep('seq', names(tab2))]))
   
-  tagcols<-factor(x = names(tagplaces), levels = names(tagplaces))
+  tagplaces[,3]<-t(as.data.frame((tab2[,grep('stt', names(tab2))])+(tab2[,grep('len', names(tab2))]/2)))
   
+  names(tagplaces)<-c('stt','seq', 'center')
+  #tagplaces<-sort_by(tagplaces,y = tagplaces$stt)
+  
+  rownames(tagplaces)<-gsub(x = rownames(tagplaces),pattern = '.stt',replacement = '')
+  
+  #tagseqs<-as.vector(tab2[,grep('seq', names(tab2))])
+  tagseqs<-t(tagplaces$seq)
+  colnames(tagseqs)<-rownames(tagplaces)
+  
+  tagplaces$center
+  #tagcenter<-(tab2[,grep('stt', names(tab2))])+(tab2[,grep('len', names(tab2))]/2)
+  tagcenter<-t(tagplaces$center)
+  colnames(tagcenter)<-rownames(tagplaces)
+  
+  tagcols<-c("#F8766D","#EA8331","#FF6A98","#39B600","#00BB4E","#00C1A3","#E76BF3","#00BAE0")
+  
+  tagcols<-tagcols[1:ncol(tagcenter)]
   
   apdf<-paste0(id,'-',setnum,'_prettyseq.pdf')
   apage<-paste0(id,'-',setnum,'_prettyseq')
+  
+  
+  
+  
+  
+  #gc()
+  
+  ## //TM CALC ####
+  
+  # Get TM from primers
+  
+  #install.packages('TmCalculator')
+  library(tibble)
+  library(reshape2)
+  library(TmCalculator)
+  
+  # concentration parameters
+  # 1X Buffer Components
+  # 20 mM Tris-HCl
+  # 10 mM (NH4)2SO4
+  # 50 mM KCl
+  # 2 mM MgSO4
+  # 0.1% Tween® 20
+  # pH 8.8@25°C
+  
+  
+  tmfun<-function(inputseq){
+    Tm_GC(inputseq,
+          ambiguous=F,
+          #variant="Primer3Plus", #Empirical constants coefficient
+          Tris=20,
+          K=50,
+          Mg = 2,
+          mismatch=F, outlist = F)}
+  
+  library(stringr)
+  library(dplyr)
+  
+  library('spgs')
+  
+  tmtab<-as.data.frame((tagseqs))
+  tmtab<-as.data.frame(t(tmtab))
+  names(tmtab)<-'seqs'
+  tmtab
+  
+  tmtab$sense<-paste0('.....',tmtab$seqs)
+  tmtab$sense<-str_extract(sequence, tmtab$sense)
+  
+  tmtab$asense<-toupper(reverseComplement(tmtab$seqs))
+  tmtab$asense<-paste0(tmtab$asense,'.....')
+  
+  
+  tmtab$sense<-str_extract(sequence, tmtab$sense)
+  tmtab$asense<-str_extract(sequence, tmtab$asense)
+  
+  
+  tmtab<-tmtab|>
+    mutate('add_5'=ifelse(
+      is.na(sense), #if is NA
+      str_sub(asense,end = -1), # keep 
+      str_sub(sense,start = 1)) # else, sub
+    )|>
+    mutate('add_4'=ifelse(
+      is.na(sense), #if is NA
+      str_sub(asense,end = -2), # keep 
+      str_sub(sense,start = 2)) # else, sub
+    )|>
+    mutate('add_3'=ifelse(
+      is.na(sense), #if is NA
+      str_sub(asense,end = -3), # keep 
+      str_sub(sense,start = 3)) # else, sub
+    )|>
+    mutate('add_2'=ifelse(
+      is.na(sense), #if is NA
+      str_sub(asense,end = -4), # keep 
+      str_sub(sense,start = 4)) # else, sub
+    )|>
+    mutate('add_1'=ifelse(
+      is.na(sense), #if is NA
+      str_sub(asense,end = -5), # keep 
+      str_sub(sense,start = 5)) # else, sub
+    )|>
+    mutate('add_0'=ifelse(
+      is.na(sense), #if is NA
+      str_sub(asense,end = -6), # keep 
+      str_sub(sense,start = 6)) # else, sub
+    )|>
+    rownames_to_column(var = 'names')
+  
+  tmtab<-tmtab|>
+    mutate('tmadd_5'=round(unlist(lapply(tmtab$add_5, tmfun)), digits = 2))|>
+    mutate('tmadd_4'=round(unlist(lapply(tmtab$add_4, tmfun)), digits = 2))|>
+    mutate('tmadd_3'=round(unlist(lapply(tmtab$add_3, tmfun)), digits = 2))|>
+    mutate('tmadd_2'=round(unlist(lapply(tmtab$add_2, tmfun)), digits = 2))|>
+    mutate('tmadd_1'=round(unlist(lapply(tmtab$add_1, tmfun)), digits = 2))|>
+    mutate('tmadd_0'=round(unlist(lapply(tmtab$add_0, tmfun)), digits = 2))
+  
+  
+  
+  library(tidyr)
   
   try(dev.off())
   
@@ -85,12 +202,12 @@ for (setnum in 'Set-9') {
     # PRIMERS SEQUENCES
     geom_label(fill='grey',hjust = 0, vjust=0,label.size = 0,label.r = unit(0,'mm'),
                label.padding = unit(-4,'mm'),family = 'mono',show.legend = F,alpha = 0,
-               aes(x = as.numeric(tagplaces)+4.45,y = .2,
-                   label = as.list(paste('',tagseqs,'')),col=tagcols), cex = 7)+
-    # PREIMERS NAMES
-    geom_label(aes(label=names(tagplaces),fill=tagcols,
+               aes(x = as.numeric(tagplaces$stt)+4.45,y = .2,
+                   label = as.list(paste('',tagseqs,''))),col=tagcols, cex = 7)+
+    # PRIMERS NAMES
+    geom_label(aes(label=rownames(tagplaces),
                    x=as.numeric(tagcenter)+4, y = .3),fontface='bold',family='mono',
-               show.legend = F, size=7)+
+               show.legend = F, size=7, fill=tagcols)+
     # TARGET HEADER
     annotate(geom = 'text', x = atitleposition, y = .4, fontface='bold',label=atitle, size =8,
              family='mono')+
@@ -103,185 +220,77 @@ for (setnum in 'Set-9') {
     theme_minimal()+
     theme(plot.margin = unit(rep(0,4), 'cm'))->aa
   
+  tmtab[,c(1,11:16)] %>%
+    gather(added, Tm, -names)|>
+    ggplot()+
+    geom_text(aes(x=added, y=Tm, label = Tm, color=Tm), size=10)+
+    facet_wrap(~names, scales = 'free')+
+    scale_color_gradient(low = 'blue', high = 'red')+
+    theme(text = element_text(size=30))-> bb
+  
+  
+  library(ggrepel)
 
-
-#gc()
-
-## //TM CALC ####
-
-# Get TM from primers
-
-#install.packages('TmCalculator')
-library(tibble)
-library(reshape2)
-library(TmCalculator)
-
-# concentration parameters
-# 1X Buffer Components
-# 20 mM Tris-HCl
-# 10 mM (NH4)2SO4
-# 50 mM KCl
-# 2 mM MgSO4
-# 0.1% Tween® 20
-# pH 8.8@25°C
-
-
-tmfun<-function(inputseq){
-  Tm_GC(inputseq,
-      ambiguous=F,
-      #variant="Primer3Plus", #Empirical constants coefficient
-      Tris=20,
-      K=50,
-      Mg = 2,
-      mismatch=F, outlist = F)}
-
-library(stringr)
-library(dplyr)
-
-library('spgs')
-
-tmtab<-as.data.frame((tagseqs))
-tmtab<-as.data.frame(t(tmtab))
-names(tmtab)<-'seqs'
-tmtab
-
-tmtab$sense<-paste0('.....',tmtab$seqs)
-tmtab$sense<-str_extract(sequence, tmtab$sense)
-
-tmtab$asense<-toupper(reverseComplement(tmtab$seqs))
-tmtab$asense<-paste0(tmtab$asense,'.....')
-
-
-tmtab$sense<-str_extract(sequence, tmtab$sense)
-tmtab$asense<-str_extract(sequence, tmtab$asense)
-
-
-tmtab<-tmtab|>
-  mutate('add_5'=ifelse(
-    is.na(sense), #if is NA
-    str_sub(asense,end = -1), # keep 
-    str_sub(sense,start = 1)) # else, sub
-    )|>
-  mutate('add_4'=ifelse(
-    is.na(sense), #if is NA
-    str_sub(asense,end = -2), # keep 
-    str_sub(sense,start = 2)) # else, sub
-    )|>
-  mutate('add_3'=ifelse(
-    is.na(sense), #if is NA
-    str_sub(asense,end = -3), # keep 
-    str_sub(sense,start = 3)) # else, sub
-    )|>
-  mutate('add_2'=ifelse(
-    is.na(sense), #if is NA
-    str_sub(asense,end = -4), # keep 
-    str_sub(sense,start = 4)) # else, sub
-    )|>
-  mutate('add_1'=ifelse(
-    is.na(sense), #if is NA
-    str_sub(asense,end = -5), # keep 
-    str_sub(sense,start = 5)) # else, sub
-    )|>
-  mutate('add_0'=ifelse(
-    is.na(sense), #if is NA
-    str_sub(asense,end = -6), # keep 
-    str_sub(sense,start = 6)) # else, sub
-    )|>
-  rownames_to_column(var = 'names')
-
-tmtab<-tmtab|>
-  mutate('tmadd_5'=round(unlist(lapply(tmtab$add_5, tmfun)), digits = 2))|>
-  mutate('tmadd_4'=round(unlist(lapply(tmtab$add_4, tmfun)), digits = 2))|>
-  mutate('tmadd_3'=round(unlist(lapply(tmtab$add_3, tmfun)), digits = 2))|>
-  mutate('tmadd_2'=round(unlist(lapply(tmtab$add_2, tmfun)), digits = 2))|>
-  mutate('tmadd_1'=round(unlist(lapply(tmtab$add_1, tmfun)), digits = 2))|>
-  mutate('tmadd_0'=round(unlist(lapply(tmtab$add_0, tmfun)), digits = 2))
-
-
-
-library(tidyr)
-
-tmtab[,c(1,11:16)] %>%
-  gather(added, Tm, -names)|>
-  ggplot()+
-  geom_text(aes(x=added, y=Tm, label = Tm, color=Tm), size=10)+
-  facet_wrap(~names, scales = 'free')+
-  scale_color_gradient(low = 'blue', high = 'red')+
-  theme(text = element_text(size=30))-> bb
-
-
-  final<-(aa / bb)+plot_layout(heights = c(1,4))
+  pdf(file = 'a.pdf', width = nchar(sequence)/5.15, height = 20,family = 'mono', title = apage)
+  
+  if (nrow(tmtab) == 8) {
+    ypos<-c(.04 ,.02 ,.02 ,.04 ,.04 ,.02 ,.04 ,.02)
+  }
+  
+  if (nrow(tmtab) == 7) {
+    ypos<-ypos[-8]
+  }
+  
+  if (nrow(tmtab) == 6) {
+    ypos<-ypos[-c(7,8)]
+  }
+  
+  a1<-aa+
+    geom_label(fill='grey',hjust = 0, vjust=0,label.size = 0,label.r = unit(0,'mm'),
+               position = position_jitter(height = .04*0, seed = 1, width = 0),
+               label.padding = unit(-4,'mm'),family = 'mono',show.legend = F,alpha = 0,
+               aes(
+                 x=ifelse(is.na(tmtab$asense),
+                          as.numeric(tagplaces$stt)+.45,
+                          as.numeric(tagplaces$stt)+5.45),
+                 
+                 y = ypos+.1,
+                 label = tmtab$add_5), cex = 7,col=tagcols)
+  
+  
+  final<-(a1 / bb)+plot_layout(heights = c(1,2))
+  
   print(final)
   dev.off()
   
 }
 
-for (totalrows in 1:nrow(tm5l)) {
-
-for (starts in 1:6) {
-#for (starts in 3) {
-  aseq<-stringr::str_sub(string = tm5l$seq[totalrows], start = starts)
-  aseq
-  tm<-round(tmfun(inputseq = aseq), digits = 2)
-  tm
-  #print(paste0(seq,'\t',tm))
-  tmtmp[starts,]$name<-tm5l$name[totalrows]
-  tmtmp[starts,]$seq<-aseq
-  tmtmp[starts,]$tm<-tm
-  tmtmp[starts,]$add<-(6-starts)
-}
-rownames(tmtmp)<-1:nrow(tmtmp)
-tmfinal<-rbind(tmfinal,tmtmp)
-}
-
-tmfinal<-tmfinal[-1,]
-rownames(tmfinal)<-1:nrow(tmfinal)
 
 
-lll
 
-lll$tm<-round(tmfun(inputseq = 'ACAGTGGAACTCCATGTGTAGCGG'), digits = 2)
-lll
+pdf(file = 'a.pdf', width = nchar(sequence)/5.15, height = 20,family = 'mono', title = apage)
 
-tmtable$tm<-lapply(tmtable$seq,tmfun)
+ypos<-(1+bitwAnd(a = 1:nrow(tmtab), b = 1))/50
 
-#plot tm
-tmtable<-tmtable[!duplicated(tmtable), ]
-tmtable$tm<-as.numeric(tmtable$tm)
-
-unique(tmtable$name)
-
-factor(tmtable$name,
-                       levels=unique(tmtable$name))
-
-tmtable$nameorder = factor(tmtable$name,
-                       levels=unique(tmtable$name))
-
-tmtable$tmround<-round(tmtable$tm, digits = 2)
+a1<-aa+
+  geom_label(fill='grey',hjust = 0, vjust=0,label.size = 0,label.r = unit(0,'mm'),
+             position = position_jitter(height = .04*0, seed = 1, width = 0),
+             label.padding = unit(-4,'mm'),family = 'mono',show.legend = F,alpha = 0,
+             aes(
+               x=ifelse(is.na(tmtab$asense),
+                        as.numeric(tagplaces)+.45,
+                        as.numeric(tagplaces)+5.45),
+               
+               y = ypos+.1,
+               label = tmtab$add_5,col=tagcols), cex = 7)
 
 
-tmtable$tmoverlap<-ifelse(
-  tmtable$overlap=="",tmtable$tmround,
-  paste0("*",tmtable$tmround,"\n",tmtable$overlap)
-)
+final<-(a1 / bb)+plot_layout(heights = c(1,2))
 
-tmtable$overlap<-ifelse(
-  tmtable$overlap=="","",
-  paste0(tmtable$name," ",tmtable$added," ",tmtable$overlap)
-)
-
-
-library(ggplot2)
-
-
-png(filename = 'test-plot.pdf', width = 1800, height = 800, res = 100)
-ggplot(data = tmfinal)+
-  theme_bw()+
-  #geom_point(cex=3,mapping = aes(y=tm, x = Added, color=tm))+
-  geom_text(cex=3,mapping = aes(y=tm, x = add, color=tm, label=tm))+
-  scale_color_gradient(low = 'black', high = 'red', name = 'Tm [ \u00B0 C ]')+
-  scale_x_continuous(breaks = -5:5, name = 'Bases added')+
-  scale_y_continuous(n.breaks = 6, name = 'Calculated Tm [ \u00B0 C ]',expand = expansion(mult = 0, add = 1.5))+
-    facet_wrap(~name, scales = 'fixed', ncol = 2)+
-  labs(title = 'Lacto set L16S1 primer Tm predict', subtitle = '* overlap F1c-B1c; B2-B3')
+print(final)
 dev.off()
+
+1:nrow(tmtab) %% 2
+
+
+
